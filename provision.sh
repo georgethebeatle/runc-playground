e9951b42d8c88d2dbaa28841df65ceadef592280
Georgi Sabev <georgethebeatle@gmail.com>
2016-06-14 15:24:29 +0300

    Add git config, config vim non-interactively

--- provision.sh
+++ provision.sh
@@ -2,43 +2,16 @@
 
 set -e -x
 
-# Use us.archive.ubuntu.com instead of EC2 mirrors, which are broken
-sed -i -e 's/http.*\.archive\.ubuntu\.com/http:\/\/us.archive.ubuntu.com/' /etc/apt/sources.list
-
-rm -rf /etc/apt/sources.list.d/multiverse-trusty*
-
-
-
 apt-get -y update
 apt-get -y clean
 
-apt-get install -y curl git gcc python-dev vim-nox jq cgroup-lite
+apt-get install -y curl git gcc make python-dev vim-nox jq cgroup-lite silversearcher-ag
 
 wget -qO- https://storage.googleapis.com/golang/go1.6.linux-amd64.tar.gz | tar -C /usr/local -xzf -
 
-#Set up $GOPATH and add go executables to $PATH
-cat > /etc/profile.d/go_env.sh <<EOF
-export GOPATH=/root/go
-export PATH=$GOPATH/bin:/usr/local/go/bin:$PATH
-EOF
-chmod +x /etc/profile.d/go_env.sh
-
-source /etc/profile.d/go_env.sh
-
-mkdir -p $GOPATH/src/github.com/opencontainers/
-pushd $GOPATH/src/github.com/opencontainers/
-git clone https://github.com/opencontainers/runc
-pushd runc
-
-GOPATH=$PWD/Godeps/_workspace:$GOPATH go build -o runc .
-cp runc /usr/local/bin/runc
-
-export UCF_FORCE_CONFFNEW=YES
-export DEBIAN_FRONTEND=noninteractive
-
-
-# Set up tmux
-wget -O /root/.tmux.conf https://raw.githubusercontent.com/luan/dotfiles/master/tmux.conf
+# Set up vim for golang development
+git clone https://github.com/luan/vimfiles.git $HOME/.vim
+$HOME/.vim/install --non-interactive
 
 # set up bash-it
 if [ ! -d ~/.bash_it ]; then
@@ -46,8 +19,44 @@ if [ ! -d ~/.bash_it ]; then
   $HOME/.bash_it/install.sh --none
 fi
 
+#Set up git aliases
+cat > $HOME/.bash_it/custom/git_config.bash <<EOF
+#!/usr/bin/env bash
+
+git config --global core.editor vim
+git config --global core.pager "less -FXRS -x2"
+
+git config --global alias.co checkout
+git config --global alias.st status
+git config --global alias.b branch
+git config --global alias.plog "log --graph --abbrev-commit --decorate --date=relative --format=format:\'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+EOF
+
+#Set up $GOPATH and add go executables to $PATH
+cat > $HOME/.bash_it/custom/go_env.bash <<EOF
+#!/usr/bin/env bash
+
+export GOPATH=/root/go
+export PATH=/root/go/bin:/usr/local/go/bin:$PATH
+EOF
+
+source $HOME/.bash_it/custom/go_env.bash
+
+RUNC_PATH=$GOPATH/src/github.com/opencontainers
+mkdir -p $RUNC_PATH
+git clone https://github.com/opencontainers/runc $RUNC_PATH/runc
+
+pushd $RUNC_PATH/runc
+  GOPATH=$PWD/Godeps/_workspace:$GOPATH go build -o runc .
+  cp runc /usr/local/bin/runc
+popd
+
+# Set up tmux
+wget -O $HOME/.tmux.conf https://raw.githubusercontent.com/luan/dotfiles/master/tmux.conf
+
 # install docker
-apt-get update && apt-get -y install apt-transport-https ca-certificates
+apt-get -y install apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 mkdir -p /etc/apt/sources.list.d
@@ -58,6 +67,4 @@ apt-cache policy docker-engine
 apt-get install -y linux-image-extra-$(uname -r) docker-engine
 service docker start
 
-# Set up vim for golang development
-curl vimfiles.luan.sh/install | bash
-
+echo Done
